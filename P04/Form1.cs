6ad269d34fbddbadfc47fdeb0af8453d0821fd17
novotnyv.03 <novotnyv.03@spst.eu>
2022-11-03 08:31:11 +0100

    P04 Hotovo

---
+++ P04/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime datum = dateTimePicker1.Value;
+            string den = Convert.ToString(datum.DayOfWeek);
+            switch (den)
+            {
+                case "Monday": MessageBox.Show(string.Format($"{den} je první pracovní den v týdnu")); break;
+                case "Tuesday": 
+                case "Wednesday":
+                case "Thursday": MessageBox.Show(string.Format($"{den} není první ani poslední pracovní den")); break;
+                case "Friday": MessageBox.Show(String.Format($"{den} je poslední pracovní den v týdnu"));break;
+                case "Saturday":
+                case "Sunday": MessageBox.Show(String.Format($"{den} je víkend"));break;
+            }
+        }
+    }
+}
